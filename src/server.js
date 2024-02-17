e7058cbf75ecd694c64ccd4bb3f58bd3871313f8
Michael Mensah <michaelmensa@live.com>
2024-02-17 07:48:23 +0000

    trying to create db

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('../routes/index')
 const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// connect to mongodb
+const uri = 'mongodb://localhost:27017/clinic_base';
+mongoose.connect(uri)
+.then(() => console.log('Connected to DB'))
+.catch((err) => console.log(err));
 
 // middleware to parse req.body in json
 app.use(bodyParser.json());
