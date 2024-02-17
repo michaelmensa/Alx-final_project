6b7a206232819a3439c6c18507fc9b1b43340dbe
michaelmensa <michaelmensa@live.com>
2024-02-17 10:14:08 +0000

    src folder created

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('../routes/index');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// middleware to parse req.body in json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Running express app on ${PORT}`);
+});
