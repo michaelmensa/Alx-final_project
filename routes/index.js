904de34ae0d1d58cf56cfc06e46577d3db9bddac
michaelmensa <michaelmensa@live.com>
2024-02-17 12:33:05 +0000

    added post route for creating clinic

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const StaticPage = require('../controllers/index');
 const AppController = require('../controllers/appController');
+const ClinicController = require('../controllers/clinicController');
 
 // get /home
 router.get('/', StaticPage.getHome);
@@ -16,7 +17,10 @@ router.get('/contact', StaticPage.getContact);
 // get /status
 router.get('/status', AppController.getStatus);
 
-// get/stats
+// get /stats
 router.get('/stats', AppController.getStats);
+
+// post /clinics
+router.post('/clinics', ClinicController.postNew);
 
 module.exports = router;
