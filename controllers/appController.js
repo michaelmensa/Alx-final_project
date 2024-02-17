d5b0c89bfa97a8a3265a49d8652b0c4761563164
michaelmensa <michaelmensa@live.com>
2024-02-17 10:12:56 +0000

    index for static page apis and appcontroller for status and stats endpoints

---
+++ controllers/appController.js
@@ -0,0 +1,29 @@
+import dbClient from '../config/db';
+
+const appController = {
+  getStatus: (req, res) => {
+    try {
+      const dbAlive = dbClient.isAlive();
+      res.status(200).json({ db: dbAlive });
+    } catch (err) {
+      res.status(500).json({ Server: `${err}` });
+    }
+  },
+
+  getStats: async (req, res) => {
+    try {
+      const countClinics = await dbClient.nbClinics();
+      const countEmployees = await dbClient.nbEmployees();
+      const countPatients = await dbClient.nbPatients();
+      res.status(200).json({
+        clinics: countClinics,
+        employees: countEmployees,
+        patients: countPatients,
+      });
+    } catch (err) {
+      res.status(500).json({ Server: `${err}` });
+    }
+  },
+};
+
+module.exports = appController;
