dd54143a82843424f3014579e410a07f026bd28c
michaelmensa <michaelmensa@live.com>
2024-02-17 20:11:49 +0000

    server.js updating middleware routes

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ const PORT = process.env.PORT || 3000;
 // middleware to parse req.body in json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   console.log(`${req.method}: ${req.url}`);
   next();
 });
 
-app.use('/', routes);
+app.use('/api/v1', routes);
 
 app.listen(PORT, () => {
   console.log(`Running express app on ${PORT}`);
