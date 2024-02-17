590c19a68cfdd386419b97d4dccd00ee8de7d808
michaelmensa <michaelmensa@live.com>
2024-02-17 16:40:21 +0000

    added middleware to log to console req.url and req.method: testing and debugging

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,13 @@
 const staticPage = {
   getHome: (req, res) => {
-    console.log(`${req.method}: ${req.url}`);
     res.status(200).json({ status: 'Home Page' });
   },
 
   getAbout: (req, res) => {
-    console.log(`${req.method}: ${req.url}`);
     res.status(200).json({ status: 'About Us Page' });
   },
 
   getContact: (req, res) => {
-    console.log(`${req.method}: ${req.url}`);
     res.status(200).json({ status: 'Contact Us Page' });
   },
 };
--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 // middleware to parse req.body in json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+  next();
+});
 
 app.use('/', routes);
 
