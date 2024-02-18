47a0a161a6f2954127fb5aee7f39c8f8749f4a75
Michael Mensah <michaelmensa@live.com>
2024-02-18 19:59:11 +0000

    updated routes and made sure to protect certain routes

--- routes/index.js
+++ routes/index.js
@@ -10,39 +10,47 @@ const EmpController = require('../controllers/empController');
 const PatientController = require('../controllers/patientController');
 
 // router.use('/dashboard') requires clinic login
-router.use('/dashboard', middleware.requireLogin);
+router.use('/clinic', middleware.requireClinicLogin);
 // define routes for /dashboard
 // get /dashboard routes
-router.get('/dashboard', ClinicController.getClinic);
-router.get('/dashboard/stats', ClinicController.getStats);
-router.get('/dashboard/employees/:id', EmpController.getShow);
-router.get('/dashboard/employees', EmpController.getIndex);
+router.get('/clinic/dashboard', ClinicController.getClinic);
+router.get('/clinic/dashboard/stats', ClinicController.getStats);
+router.get('/clinic/dashboard/employees/:id', EmpController.getShow);
+router.get('/clinic/dashboard/employees', EmpController.getIndex);
+router.get('/clinic/dashboard/loginemployee', EmpController.getEmployeeLogIn);
 // post /dashboard/employees
-router.post('/dashboard/employees', EmpController.postNew);
+router.post('/clinic/dashboard/employees', EmpController.postNew);
+router.post('/clinic/dashboard/loginemployee', EmpController.postEmployeeLogIn);
+// post /dashboard/logout to log out clinic
+router.post('/clinic/dashboard/logout', AppController.postLogOut);
 
+// router.use('/dashboard/employee/dashboard') all requires employee login
+router.use('/employee', middleware.requireEmployeeLogin);
+router.get('/employee/dashboard', EmpController.getEmployee);
+router.post('/employee/dashboard/logout', EmpController.postLogOut);
+
+// routes that are not protected
 // get /home
 router.get('/', StaticPage.getHome);
-
 // get /about
 router.get('/about', StaticPage.getAbout);
-
 // get /contact
 router.get('/contact', StaticPage.getContact);
-
 // get /status
 router.get('/status', AppController.getStatus);
-
 // get /stats
 router.get('/stats', AppController.getStats);
+// get /auth/register to get register new clinic page
+router.get('/auth/register', AppController.getSignUp);
+// get /auth/login to get log in page after clinic has been registered
+router.get('/auth/login', AppController.getLogIn);
 
 // post requests
-// post /auth/register
+// post /auth/register to register clinic
 router.post('/auth/register', ClinicController.postNew);
-
-// post /patients
-router.post('/patients', PatientController.postNew);
-
-// post /auth/login
+// post /auth/login to log in to clinic dashboard
 router.post('/auth/login', ClinicController.postClinic);
+// post /patients to create patients
+router.post('/patients', PatientController.postNew);
 
 module.exports = router;
