929b0944bb7de546abaa53b92889ec464e790684
Michael Mensah <michaelmensa@live.com>
2024-02-18 19:58:00 +0000

    middlewares to authenticate employee sessions

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,10 +1,20 @@
 const middleware = {
-  requireLogin: (req, res, next) => {
-    console.log(req.session.user);
-    if (req.session && req.session.user) {
+  requireClinicLogin: (req, res, next) => {
+    console.log(req.session.clinic);
+    if (req.session && req.session.clinic) {
       next();
     } else {
       res.status(401).json({ error: 'Login required' });
+    }
+  },
+
+  requireEmployeeLogin: (req, res, next) => {
+    console.log(req.session.clinic.employee);
+    if (!req.session && !req.session.clinic) {
+      res.status(401).json({ error: 'Login required' });
+    }
+    if (req.session.clinic.employee) {
+      next();
     }
   },
 };
