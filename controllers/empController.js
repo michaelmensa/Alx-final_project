d142328e6406a79886a6db4add5fdc61a1dbb09a
Michael Mensah <michaelmensa@live.com>
2024-02-18 19:51:27 +0000

    using express-session to create authentication for employees

--- controllers/empController.js
+++ controllers/empController.js
@@ -1,4 +1,5 @@
 import dbClient from '../config/db';
+import utils from '../utils/utils';
 
 const empController = {
   postNew: async (req, res) => {
@@ -42,26 +43,23 @@ const empController = {
 
   getShow: async (req, res) => {
     // query by type and page number for pagination
-    const clinicName = req.session.user.name;
-    const id = req.params.id;
-    const clinicId = req.session.user.id;
-    const matchCondition = { clinic_id: clinicId };
+    const { id } = req.params;
+    const clinicId = req.session.clinic.id;
+    const matchCondition = { clinicId };
     const pipeline = [
-      { $match: matchCondition }
+      { $match: matchCondition },
     ];
     const employees = await dbClient.db.collection('employees').aggregate(pipeline).toArray();
-    const employee = employees.find(emp => emp._id.toString() === id);
+    const employee = employees.find((emp) => emp._id.toString() === id);
     res.status(200).json(employee);
   },
 
   getIndex: async (req, res) => {
     // query by type and page number for pagination
-    const clinicName = req.session.user.name;
-    const clinicId = req.session.user.id;
+    const clinicId = req.session.clinic.id;
     const page = req.query.page ? parseInt(req.query.page, 10) : 0;
-    const clinic = await dbClient.findClinicByField('name', clinicName);
     const skip = page * 10;
-    const matchCondition = { clinic_id: clinicId };
+    const matchCondition = { clinicId };
     const pipeline = [
       { $match: matchCondition },
       { $skip: skip },
@@ -69,6 +67,60 @@ const empController = {
     ];
     const employees = await dbClient.db.collection('employees').aggregate(pipeline).toArray();
     res.status(200).json(employees);
+  },
+
+  getEmployeeLogIn: (req, res) => {
+    res.send('Employee Login Page');
+  },
+
+  postEmployeeLogIn: async (req, res) => {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    // validate email and password
+    if (!email) {
+      res.status(400).json({ error: 'email required' });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: 'password required' });
+      return;
+    }
+
+    // authenticate login
+    // check if clinic exists in db
+    try {
+      const employee = await dbClient.findEmp(email);
+      if (!employee) {
+        res.status(404).json({ error: 'Not found' });
+        return;
+      }
+      const isValid = utils.checkPassword(password, employee.password);
+      if (!isValid) {
+        res.status(401).json({ message: 'check email or password' });
+        return;
+      }
+      // store employee session as object within clinic session
+      req.session.clinic.employee = {
+        id: employee._id.toString(),
+        name: employee.name,
+      };
+      res.status(201).json({ message: 'Employee log in success' });
+    } catch (err) {
+      res.status(500).json({ error: `${err}` });
+    }
+  },
+
+  getEmployee: (req, res) => {
+    res.send(`Welcome ${req.session.clinic.employee.name}`);
+  },
+
+  postLogOut: (req, res) => {
+    if (!req.session.clinic && !req.session.clinic.employee) {
+      res.status(404).json({ error: 'Not found' });
+    }
+    delete req.session.clinic.employee;
+    res.redirect('/api/v1/clinic/dashboard');
   },
 };
 
