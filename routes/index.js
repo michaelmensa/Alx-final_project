e459b00e422bf7ac24b45b2825ddd07006fdde42
Michael Mensah <michaelmensa@live.com>
2024-02-19 16:03:34 +0000

    improved routing for clarity

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,25 @@ const ClinicController = require('../controllers/clinicController');
 const EmpController = require('../controllers/empController');
 const PatientController = require('../controllers/patientController');
 
-// router.use('/dashboard') requires clinic login
+// router.use('/clinic') requires clinic login
 router.use('/clinic', middleware.requireClinicLogin);
-// define routes for /dashboard
-// get /dashboard routes
+// define routes for /clinic/dashboard
+// get /clinic/dashboard routes
 router.get('/clinic/dashboard', ClinicController.getClinic);
 router.get('/clinic/dashboard/stats', ClinicController.getStats);
 router.get('/clinic/dashboard/employees/:id', EmpController.getShow);
 router.get('/clinic/dashboard/employees', EmpController.getIndex);
 router.get('/clinic/dashboard/loginemployee', EmpController.getEmployeeLogIn);
-// post /dashboard/employees
+// post /clinic/dashboard/employees
 router.post('/clinic/dashboard/employees', EmpController.postNew);
 router.post('/clinic/dashboard/loginemployee', EmpController.postEmployeeLogIn);
-// post /dashboard/logout to log out clinic
+// post /clinic/dashboard/logout to log out clinic
 router.post('/clinic/dashboard/logout', AppController.postLogOut);
 
-// router.use('/dashboard/employee/dashboard') all requires employee login
+// router.use('/employee/dashboard') all requires employee login
 router.use('/employee', middleware.requireEmployeeLogin);
 router.get('/employee/dashboard', EmpController.getEmployee);
+router.post('/employee/dashboard/createpatient', PatientController.postNew);
 router.post('/employee/dashboard/logout', EmpController.postLogOut);
 
 // routes that are not protected
@@ -36,8 +37,6 @@ router.get('/', StaticPage.getHome);
 router.get('/about', StaticPage.getAbout);
 // get /contact
 router.get('/contact', StaticPage.getContact);
-// get /status
-router.get('/status', AppController.getStatus);
 // get /stats
 router.get('/stats', AppController.getStats);
 // get /auth/register to get register new clinic page
