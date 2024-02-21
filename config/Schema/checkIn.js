561f5be96803305112580a833f796141896ea1b4
Michael Mensah <michaelmensa@live.com>
2024-02-21 18:08:42 +0000

    checkin.js to store checkins/examinations

---
+++ config/Schema/checkIn.js
@@ -0,0 +1,29 @@
+import Examination from './examination';
+
+const mongoose = require('mongoose');
+
+/**
+ * Examination is imported to be a subdocument in
+ * the checkInSchema
+ */
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const checkInSchema = new mongoose.Schema({
+  patientId: {
+    type: ObjectId,
+    ref: 'Patient',
+    required: true,
+  },
+  visitDate: {
+    type: Date,
+    default: Date.now,
+  },
+  examination: {
+    type: Examination,
+  },
+});
+
+const CheckIn = mongoose.model('CheckIn', checkInSchema);
+
+module.exports = CheckIn;
